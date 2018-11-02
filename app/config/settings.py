50659fbf06e09281c7597aba2c0159a9cb3189da
pmb <gambler1541@gmail.com>
2018-11-02 13:26:33 +0900

    MEDIA_URL, MEDIA_ROOT설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,7 +14,18 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(f'BASE_DIR: {BASE_DIR}')
+ROOT_DIR = os.path.dirname(BASE_DIR)
+print(f'ROOT_DIR: {ROOT_DIR}')
+# 사용자가 업로드한 파일이 저장될 Base디렉토리 (settings.MEDIA_ROOT)
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+print(f'MEDIA_ROOT: {MEDIA_ROOT}')
 
+# 유저가 업로드한 파일에 접근하고자 할 때의 prefix URL(정적 URL, settings.MEDIA_URL)
+# FileField, MediaField의 URL이 아래 설정 기준으로 바뀜
+MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,4 +132,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
