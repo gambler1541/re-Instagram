8a8e0eacb5945b5df964ada7444c9908b80dfe52
pmb <gambler1541@gmail.com>
2018-11-02 11:51:10 +0900

    django admin페이지에 User,Comment,Hash Tag, Posts추가

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment, HashTag
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(HashTag)
