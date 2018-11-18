ab73349d06d100151c541f5e157e71ccd20f2c8b
pmb <gambler1541@gmail.com>
2018-11-18 22:27:44 +0900

    signup 중복render 제거

--- app/members/views.py
+++ app/members/views.py
@@ -28,25 +28,53 @@ def logout_view(request):
         return redirect('posts:post_list')
 
 def signup_view(request):
+    # render하는 경우
+    #   1. POST요청이며, 사용자명이 이미 존재할 경우
+    #   2. POST요청이며, 비밀번호가 같지 않은 경우
+    #   3. GET요청인 경우
+    # redirect하는 경우
+    #   1. POST요청이며, 사용자명이 존재하지 않고 비밀번호가 같은 경우
+    """
+    if request.method가 POST면:
+        if 사용자명이 존재하면:
+            render
+        if 비밀번호가 같지 않으면:
+            render
+        (else, POST면서 사용자도없고 비밀번호도 같으면):
+            redirect
+    (else, GET요청이면):
+        render
+
+    if request.method가 POST면:
+        if 사용자명이 존재하면:
+        if 비밀번호가 같지 않으면:
+        (else, POST면서 사용자도없고 비밀번호도 같으면):
+            redirect
+    (else, GET요청이면):
+        render
+
+
+    :return:
+    """
+    context ={
+        'form':SignupForm(),
+    }
     if request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        if User.objects.filter(username=username).exists():
-            return HttpResponse(f'{username}은 이미 사용중입니다.')
-        if password1 != password2:
-            return HttpResponse('비밀번호를 확인하여 주십시요.')
 
-        user = User.objects.create_user(
-            username=username,
-            password=password1,
-        )
-        login(request, user)
-        return redirect('posts:post_list')
-    else:
-        form = SignupForm()
-        context = {
-            'form' : form
-        }
-        return render(request, 'members/signup.html', context)
+        if User.objects.filter(username=username).exists():
+            context['error'] = f'{username}은 이미 사용중입니다.'
+        elif password1 != password2:
+            context['error'] = '비밀번호를 확인해 주세요.'
+        else:
+            user = User.objects.create_user(
+                username=username,
+                password=password1,
+            )
+            login(request, user)
+            return redirect('posts:post_list')
+
+    return render(request, 'members/signup.html', context)
